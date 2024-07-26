39d7a2e1ad83c7990dfdbcdf93f32ec84b5d5c3d
piquark6046 <j.w.so@adguard.com>
2024-07-26 10:41:00 +0000

    fix: use glob instead

--- sources/token.ts
+++ sources/token.ts
@@ -1,32 +1,10 @@
 import * as Fs from 'node:fs'
+import * as Glob from 'glob'
 
 export function LoadTokens(RepoPath: string) {
   const Tokens = new Map<string, string>()
-  for (const Selected of ListFiles(RepoPath)) {
-    Tokens.set(Selected, Fs.readFileSync(Selected, 'utf-8'))
+  for (const Selected of Glob.globSync('**/*.token', { cwd: RepoPath })) {
+    Tokens.set(Selected, Fs.readFileSync(`${RepoPath}/${Selected}`, 'utf-8'))
   }
   return Tokens
-}
-
-function ListFiles(RepoPath: string): string[] {
-  const Files: string[] = []
-  const Paths: string[] = [RepoPath]
-
-  while (Paths.length > 0) {
-    const CurrentPath = Paths.shift()!
-    const DirContents = Fs.readdirSync(CurrentPath)
-
-    DirContents.forEach((Item) => {
-      const ItemPath = `${CurrentPath}/${Item}`
-      const IsDirectory = Fs.statSync(ItemPath).isDirectory()
-
-      if (IsDirectory) {
-        Paths.push(ItemPath)
-      } else {
-        Paths.push(ItemPath)
-      }
-    })
-  }
-
-  return Files
 }
