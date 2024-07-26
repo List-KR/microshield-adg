09832d225bc0c02b47a85182af0cf95cbf0fa65f
piquark6046 <j.w.so@adguard.com>
2024-07-26 07:57:52 +0000

    feat: export tokens as Map instance

--- sources/token.ts
+++ sources/token.ts
@@ -2,16 +2,31 @@ import * as Fs from 'node:fs'
 
 export function LoadTokens(RepoPath: string) {
   const Tokens = new Map<string, string>()
-  
+  for (const Selected of ListFiles(RepoPath)) {
+    Tokens.set(Selected, Fs.readFileSync(Selected, 'utf-8'))
+  }
   return Tokens
 }
 
-function ListFiles(DirPath: string): string[] {
-  for (const Path of Fs.readdirSync(DirPath)) {
-    if ()
-  }
-}
+function ListFiles(RepoPath: string): string[] {
+  const Files: string[] = []
+  const Paths: string[] = [RepoPath]
 
-function ListDir(Paths: string[]): string[] {
-  
+  while (Paths.length > 0) {
+    const CurrentPath = Paths.shift()!
+    const DirContents = Fs.readdirSync(CurrentPath)
+
+    DirContents.forEach((Item) => {
+      const ItemPath = `${CurrentPath}/${Item}`
+      const IsDirectory = Fs.statSync(ItemPath).isDirectory()
+
+      if (IsDirectory) {
+        Paths.push(ItemPath)
+      } else {
+        Paths.push(ItemPath)
+      }
+    })
+  }
+
+  return Files
 }
